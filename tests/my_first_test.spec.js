3c464591bab2d75394266eb3cf641f3fd4c63683
Lola <104280935+Omololaomotalade@users.noreply.github.com>
2025-08-13 12:26:43 +0100

    Update my_first_test.spec.js

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -6,8 +6,8 @@ test('My first test', async ({page}) => {
    await page.pause()
    //await expect(page).toHaveTitle('Login | KongaPay Business Portal')
    //await page.waitForSelector('input[placeholder="e.g. johndoe123"]');
-   await page.locator('[placeholder="e.g. johndoe123"]').fill('abdulazeezbarkey');
-   await page.locator('[placeholder="Password"]').fill('Barkey2024#')
+   await page.locator('[placeholder="e.g. johndoe123"]').fill('');
+   await page.locator('[placeholder="Password"]').fill('#')
    //await page.waitForSelector('button', { name: 'Login' }, {timeout: 5000})
    await page.getByRole('button', { name: 'Login' }).click();
    await page.goto('https://business.kongapay.com/dashboard');
